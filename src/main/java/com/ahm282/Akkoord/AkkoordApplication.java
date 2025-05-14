bc0452e527909e1c9264c51ba94f7862206364ec
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-05-14 23:51:47 +0300

    Initial commit

---
+++ src/main/java/com/ahm282/Akkoord/AkkoordApplication.java
@@ -0,0 +1,13 @@
+package com.ahm282.Akkoord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkoordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkoordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahm282/Akkoord/AkkoordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahm282.Akkoord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkkoordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
