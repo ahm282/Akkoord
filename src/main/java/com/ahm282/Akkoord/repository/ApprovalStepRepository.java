b8d35d6deebfd54f6087e8ec8a4e5d3733d34b17
Ahmed Mahgoub <ahmedhosny282@gmail.com>
2025-05-17 14:54:44 +0300

    refactor: update ApprovalStep and AuditLog repositories

--- src/main/java/com/ahm282/Akkoord/repository/ApprovalStepRepository.java
+++ src/main/java/com/ahm282/Akkoord/repository/ApprovalStepRepository.java
@@ -2,7 +2,7 @@ package com.ahm282.Akkoord.repository;
 
 import com.ahm282.Akkoord.model.entity.AppUser;
 import com.ahm282.Akkoord.model.entity.ApprovalStep;
-import com.ahm282.Akkoord.model.entity.Document;
+import com.ahm282.Akkoord.model.entity.DocumentVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,8 +10,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface ApprovalStepRepository extends JpaRepository<ApprovalStep, UUID> {
-    List<ApprovalStep> findByDocument(Document document);
+    List<ApprovalStep> findByDocumentVersion(DocumentVersion documentVersion);
     List<ApprovalStep> findByAssignedUser(AppUser user);
     List<ApprovalStep> findByCreatedBy(AppUser user);
-    Optional<ApprovalStep> findByDocumentAndStepOrder(Document document, Integer stepOrder);
+    Optional<ApprovalStep> findByDocumentVersionAfterAndStepOrder(DocumentVersion documentVersion, Integer stepOrder);
 }
--- src/main/java/com/ahm282/Akkoord/repository/AuditLogRepository.java
+++ src/main/java/com/ahm282/Akkoord/repository/AuditLogRepository.java
@@ -3,6 +3,8 @@ package com.ahm282.Akkoord.repository;
 import com.ahm282.Akkoord.model.entity.AppUser;
 import com.ahm282.Akkoord.model.entity.AuditLog;
 import com.ahm282.Akkoord.model.entity.Document;
+import com.ahm282.Akkoord.model.enums.AuditAction;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -11,7 +13,7 @@ import java.util.UUID;
 
 public interface AuditLogRepository extends JpaRepository<AuditLog, UUID> {
     List<AuditLog> findByDocument(Document document);
-    List<AuditLog> findByUser(AppUser user);
-    List<AuditLog> findByActionType(String actionType);
-    List<AuditLog> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
+    List<AuditLog> findByActor(AppUser user);
+    List<AuditLog> findByAction(AuditAction action);
+    List<AuditLog> findByCreatedAtBetween(LocalDateTime createdAtBefore, LocalDateTime createdAtAfter, Limit limit);
 }
